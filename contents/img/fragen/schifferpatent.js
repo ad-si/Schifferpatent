6a6aad0a376c177bbeb960e062854917aaa3826f
Adrian Sieber <mail@adriansieber.com>
2015-05-16 17:09:38 +0200

    Add graphic 428

--- contents/img/fragen/schifferpatent.js
+++ contents/img/fragen/schifferpatent.js
@@ -893,6 +893,41 @@ module.exports.shaven = function (conf, tools) {
 				},
 				'xlink:href': '#sailingBoat'
 			}]
+		],
+		['g.exerciseImage', conf.number === 428,
+			['use', {'xlink:href': '#water'}],
+			['g',
+				{
+					transform: 'translate(10 55)'
+				},
+				['line', {
+					style: {
+						'marker-end': 'url(#endMarkerArrow)'
+					},
+					x2: 50
+				}],
+				['text', 'Wind', {
+					style: {
+						'font-size': 11
+					},
+					x: 8,
+					y: -4
+				}]
+			],
+			['use', {
+				transform: 'translate(120 30) rotate(10) scale(-1 1)',
+				style: {
+					fill: 'black'
+				},
+				'xlink:href': '#sailingBoat'
+			}],
+			['use', {
+				transform: 'translate(100, 60) rotate(150)',
+				style: {
+					fill: 'white'
+				},
+				'xlink:href': '#sailingBoat'
+			}]
 		]
 	]
 }
